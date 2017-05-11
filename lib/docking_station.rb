d05e7af030776cb3b8047c2bffc5d21f255b41c5
Panteha <panteha.ahmadi@gmail.com>
2017-05-11 13:38:08 +0100

    Raise error if we release more than one bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,11 +4,13 @@ require_relative 'bike'
 class DockingStation
 
   attr_reader :bike
+
   def release_a_bike
-    Bike.new
+    fail "No more bikes!" unless @bike
+    
   end
 
   def dock_a_bike(bike)
-    bike
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,7 @@ describe DockingStation  do
   it { is_expected.to respond_to(:dock_a_bike).with(1).argument }
 
   it 'Releases working bikes' do
-    bike = subject.release_a_bike
+    bike = Bike.new
     expect(bike).to be_bike_is_working
   end
 
@@ -20,6 +20,6 @@ describe DockingStation  do
 
   it "will raise an error when more than one bike is released" do
     bike = Bike.new
-    expect {bike = Bike.new}.to raise_error("No more bikes!")
+    expect {subject.release_a_bike}.to raise_error("No more bikes!")
   end
 end
